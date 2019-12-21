d39885dba27f34c9c9c641480f215161da184a33
alissao <danvil54@hotmail.com>
2019-12-21 19:25:06 -0300

    Spiral matrix new attempt

--- src/main/java/leetcode/arrayAndString/SpiralMatrix.java
+++ src/main/java/leetcode/arrayAndString/SpiralMatrix.java
@@ -2,40 +2,59 @@ package leetcode.arrayAndString;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class SpiralMatrix {
 
+    public static void main(String[] args) {
+        System.out.println("Sua matriz vai ser de x por x. Insira esses dois números: ");
+        Scanner scan = new Scanner(System.in);
+        int r = scan.nextInt();
+        int c = scan.nextInt();
+        int matriz[][] = new int[r][c];
+        System.out.println("Insira os números da sua matriz na mesma linha: ");
+        List<Integer> numMatriz = new ArrayList<Integer>();
+        try {
+            for (int i = 0; i < (r*c); i++) {
+                numMatriz.add(scan.nextInt());
+            }
+        }catch (Exception e){
+            System.out.println("Houve algo de errado com os números que você digitou.");
+        }
+    }
+
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> result = new ArrayList<Integer>();
-        int row = 0;
-        int col = 0;
-        int numsInMatrix = matrix.length*matrix[0].length;
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        boolean[][] passedBy = new boolean[matrix.length][matrix[0].length];
-        Movement mov = new Movement();
 
         if (matrix.length == 0) {
             return result;
         }
 
-        for (int k = 0; k < numsInMatrix; k++){
+        int row = 0;
+        int col = 0;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int numbsInMatrix = rows*cols;
+        boolean[][] passedBy = new boolean[rows][cols];
+        Movement mov = new Movement();
+
+        for (int k = 0; k < numbsInMatrix; k++){
             result.add(matrix[row][col]);
 
             /* this condition below is for checking the limits and also
              * if the spiral passed by the next number from the matrix.
              */
             if (((0 <= row && row < rows) && (0 <= col && col < cols)) &&
-                ((mov.movingSpiral == 1 && !passedBy[row][col+1])
-                || (mov.movingSpiral == 2 && !passedBy[row+1][col])
-                || (mov.movingSpiral == 3 && !passedBy[row][col-1])
-                || (mov.movingSpiral == 4 && !passedBy[row-1][col]))) {
-                passedBy[row][col] = true;
+                ((mov.getMovingSpiral() == 1 && !passedBy[row][col+1])
+                || (mov.getMovingSpiral() == 2 && !passedBy[row+1][col])
+                || (mov.getMovingSpiral() == 3 && !passedBy[row][col-1])
+                || (mov.getMovingSpiral() == 4 && !passedBy[row-1][col]))) {
+                    passedBy[row][col] = true;
             }else {
                 passedBy[row][col] = true;
                 mov.changeMovement();
             }
-            switch (mov.movingSpiral) {
+            switch (mov.getMovingSpiral()) {
                 case 1:
                     col += 1;
                 case 2:
@@ -49,8 +68,13 @@ public class SpiralMatrix {
         return result;
     }
 
-    public class Movement {
-        protected int movingSpiral = 1;
+    public static class Movement {
+        private int movingSpiral = 1;
+
+        public int getMovingSpiral() {
+            return movingSpiral;
+        }
+
         private void changeMovement() {
             if (this.movingSpiral == 4) {
                 this.movingSpiral = 1;
