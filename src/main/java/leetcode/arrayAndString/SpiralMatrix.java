cbe7976e12d18dd008c73189a3b46d903c960db6
alissao <danvil54@hotmail.com>
2019-12-31 16:57:14 -0300

    Spiral Matrix done

--- src/main/java/leetcode/arrayAndString/SpiralMatrix.java
+++ src/main/java/leetcode/arrayAndString/SpiralMatrix.java
@@ -15,12 +15,18 @@ public class SpiralMatrix {
         System.out.println("Insira os números da sua matriz na mesma linha: ");
         List<Integer> numMatriz = new ArrayList<Integer>();
         try {
-            for (int i = 0; i < (r*c); i++) {
-                numMatriz.add(scan.nextInt());
+            for (int i = 0; i < r; i++) {
+                for (int j = 0; j < c; j++){
+                    matriz[i][j] = scan.nextInt();
+                }
             }
         }catch (Exception e){
             System.out.println("Houve algo de errado com os números que você digitou.");
         }
+        SpiralMatrix spiral = new SpiralMatrix();
+        numMatriz = spiral.spiralOrder(matriz);
+        System.out.println("Sua matriz em espiral é: ");
+        System.out.println(numMatriz);
     }
 
     public List<Integer> spiralOrder(int[][] matrix) {
@@ -41,28 +47,22 @@ public class SpiralMatrix {
         for (int k = 0; k < numbsInMatrix; k++){
             result.add(matrix[row][col]);
 
-            /* this condition below is for checking the limits and also
-             * if the spiral passed by the next number from the matrix.
-             */
-            if (((0 <= row && row < rows) && (0 <= col && col < cols)) &&
-                ((mov.getMovingSpiral() == 1 && !passedBy[row][col+1])
-                || (mov.getMovingSpiral() == 2 && !passedBy[row+1][col])
-                || (mov.getMovingSpiral() == 3 && !passedBy[row][col-1])
-                || (mov.getMovingSpiral() == 4 && !passedBy[row-1][col]))) {
-                    passedBy[row][col] = true;
-            }else {
-                passedBy[row][col] = true;
-                mov.changeMovement();
-            }
+            //checks the limits and changes the movement
+            mov.isOnTheLimit(row,col,rows,cols,passedBy);
+
             switch (mov.getMovingSpiral()) {
                 case 1:
                     col += 1;
+                    break;
                 case 2:
                     row += 1;
+                    break;
                 case 3:
                     col -= 1;
+                    break;
                 case 4:
                     row -= 1;
+                    break;
             }
         }
         return result;
@@ -82,6 +82,53 @@ public class SpiralMatrix {
                 this.movingSpiral += 1;
             }
         }
+
+        private void isOnTheLimit (int row,int col, int rows, int cols, boolean[][] passedBy){
+            switch (this.movingSpiral){
+                case 1:
+                    if (col == cols-1){
+                        changeMovement();
+                        break;
+                    }else{
+                        if (passedBy[row][col+1]){
+                            changeMovement();
+                        }
+                        break;
+                    }
+                case 2:
+                    if (row == rows-1){
+                        changeMovement();
+                        break;
+                    }else{
+                        if (passedBy[row+1][col]){
+                            changeMovement();
+                        }
+                        break;
+                    }
+                case 3:
+                    if (col == 0){
+                        changeMovement();
+                        break;
+                    }else{
+                        if (passedBy[row][col-1]){
+                            changeMovement();
+                        }
+                        break;
+                    }
+                case 4:
+                    if (row == 0){
+                        changeMovement();
+                        break;
+                    }else{
+                        if (passedBy[row-1][col]){
+                            changeMovement();
+                        }
+                        break;
+                    }
+            }
+            passedBy[row][col] = true;
+        }
+
         public Movement(){}
     }
 }
